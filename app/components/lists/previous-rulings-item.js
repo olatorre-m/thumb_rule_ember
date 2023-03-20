fc3b0b1cc931772ec2e67c3bec28002989788610
Daniela Latorre <53132542+olatorre-m@users.noreply.github.com>
2023-03-20 00:18:50 -0500

    Added helpers to get percentages.

---
+++ app/components/lists/previous-rulings-item.js
@@ -0,0 +1,29 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class PreviousRulingItemComponent extends Component {
+  @tracked person;
+  @tracked voted = false;
+
+  constructor(...args) {
+    super(...args);
+
+    if (this.args.person) {
+      this.person = this.args.person;
+    }
+  }
+
+  get positivePercentage() {
+    return (
+      (this.person.votes.positive * 100) /
+      (this.person.votes.positive + this.person.votes.negative)
+    ).toFixed(1);
+  }
+
+  get negativePercentage() {
+    return (
+      (this.person.votes.negative * 100) /
+      (this.person.votes.positive + this.person.votes.negative)
+    ).toFixed(1);
+  }
+}
