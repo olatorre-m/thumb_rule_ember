c2f23986d5aa707de0665c1bc688fc9fa6d94138
Daniela Latorre <53132542+olatorre-m@users.noreply.github.com>
2023-03-20 00:41:38 -0500

    Added date from now using a helper.

--- app/components/lists/previous-rulings-item.js
+++ app/components/lists/previous-rulings-item.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import moment from 'moment';
 
 export default class PreviousRulingItemComponent extends Component {
   @tracked person;
@@ -25,5 +26,9 @@ export default class PreviousRulingItemComponent extends Component {
       (this.person.votes.negative * 100) /
       (this.person.votes.positive + this.person.votes.negative)
     ).toFixed(1);
+  }
+
+  get timeFromNow() {
+    return moment(this.person.lastUpdated).fromNow();
   }
 }
