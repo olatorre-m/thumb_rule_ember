2ef637f18150afe3a5d3416048daf5b3efe9e809
Daniela Latorre <53132542+olatorre-m@users.noreply.github.com>
2023-03-21 00:36:14 -0500

    Added classes for grid and list view.

--- app/components/sections/previous-rulings-section.js
+++ app/components/sections/previous-rulings-section.js
@@ -5,7 +5,7 @@ import { inject as service } from '@ember/service';
 
 export default class PreviousRulingSectionComponent extends Component {
   @tracked data;
-  @tracked view = 'list';
+  @tracked view = 'grid';
   @service media;
 
   constructor(...args) {
