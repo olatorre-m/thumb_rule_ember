e1f5fe9f4c0a6dda41bf9c423becf5e0a057e786
Daniela Latorre <53132542+olatorre-m@users.noreply.github.com>
2023-03-21 03:38:57 -0500

    Added test folder

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'thumb-rule/app';
+import config from 'thumb-rule/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
